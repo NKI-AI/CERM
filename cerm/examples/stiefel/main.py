f010f92191347cb441bbdf9ad6464230596373c2
RaySheombarsing <r.s.s.sheombarsing@outlook.com>
2023-09-19 12:16:31 +0200

    constrained optimization

--- cerm/examples/stiefel/main.py
+++ cerm/examples/stiefel/main.py
@@ -1,16 +1,20 @@
 import os
 import sys
 
-sys.path.append(os.getcwd()) 
+sys.path.append(os.getcwd())
 
 import hydra
 import torch
+
 from omegaconf import DictConfig
 from typing import Optional
 from logging import getLogger
 from sklearn.metrics import balanced_accuracy_score
 
 from cerm.examples.stiefel.data.mnist_data import create_mnist_datasets
+from cerm.examples.stiefel.data.cifar100_data import create_cifar_datasets
+from cerm.optimizer.riemannian_sgd import RSGD
+from cerm.network.constrained_params import split_params
 
 logger = getLogger(__name__)
 
@@ -20,29 +24,45 @@ def main(cfg: DictConfig) -> Optional[float]:
     # use gpu if available
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     logger.info(f"Using device: {device}")
-    
+
     # Create datasets and loaders
-    train_dataset, val_dataset, test_dataset = create_mnist_datasets(
+    train_dataset, val_dataset, test_dataset = create_cifar_datasets(
         cfg.data.path, cfg.data.random_train_val_split
     )
 
     train_dataloader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=cfg.data.batch_size, shuffle=True, num_workers=cfg.data.num_workers
+        train_dataset,
+        batch_size=cfg.data.batch_size,
+        shuffle=True,
+        num_workers=cfg.data.num_workers,
     )
     val_dataloader = torch.utils.data.DataLoader(
-        val_dataset, batch_size=cfg.data.batch_size, shuffle=False, num_workers=cfg.data.num_workers
+        val_dataset,
+        batch_size=cfg.data.batch_size,
+        shuffle=False,
+        num_workers=cfg.data.num_workers,
     )
     test_dataloader = torch.utils.data.DataLoader(
-        test_dataset, batch_size=cfg.data.batch_size, shuffle=False, num_workers=cfg.data.num_workers
+        test_dataset,
+        batch_size=cfg.data.batch_size,
+        shuffle=False,
+        num_workers=cfg.data.num_workers,
     )
 
     # Instantiate model
     logger.info(f"Instantiating network {cfg.network._target_}")
     model = hydra.utils.instantiate(cfg.network).to(device)
+    unconstrained_params, constrained_params = split_params(model)
 
-    # Instantiate optimizer
-    logger.info(f"Instantiating optimizer {cfg.optimizer._target_}")
-    optimizer = hydra.utils.instantiate(cfg.optimizer, model.parameters())
+    logger.info(f"Instantiating optimizer {cfg.unconstrained_optimizer._target_}")
+    unconstrained_optim = hydra.utils.instantiate(
+        cfg.unconstrained_optimizer, unconstrained_params
+    )
+
+    logger.info(f"Instantiating optimizer {cfg.constrained_optimizer._target_}")
+    constrained_optim = hydra.utils.instantiate(
+        cfg.constrained_optimizer, constrained_params
+    )
 
     # Instantiate loss
     logger.info(f"Instantiating loss {cfg.loss._target_}")
@@ -57,12 +77,15 @@ def main(cfg: DictConfig) -> Optional[float]:
         y_train_pred = []
 
         for batch_idx, (data, target) in enumerate(train_dataloader):
+            unconstrained_optim.zero_grad()
+            constrained_optim.zero_grad()
             data, target = data.to(device), target.to(device)
-            optimizer.zero_grad()
             output = model(data)
             loss_value = loss(output, target)
             loss_value.backward()
-            optimizer.step()
+            with torch.no_grad():
+                unconstrained_optim.step()
+                constrained_optim.step()
 
             pred = output.argmax(dim=1, keepdim=True).squeeze().cpu().numpy()
             y_train_true.extend(target.cpu().numpy())
@@ -70,13 +93,15 @@ def main(cfg: DictConfig) -> Optional[float]:
 
             total_train_loss += loss_value.item()
             num_train_batches += 1
-            
+
             if batch_idx % cfg.training.log_interval == 0:
-                logger.info(f"Epoch: {epoch} | Batch: {batch_idx} | Training loss: {loss_value.item():.4f}")
+                logger.info(
+                    f"Epoch: {epoch} | Batch: {batch_idx} | Training loss: {loss_value.item():.4f}"
+                )
 
         avg_train_loss = total_train_loss / num_train_batches
         train_balanced_accuracy = balanced_accuracy_score(y_train_true, y_train_pred)
-        
+
         # Validation
         model.eval()
         total_val_loss = 0
